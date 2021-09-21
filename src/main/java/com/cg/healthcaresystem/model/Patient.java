bd48003702e82add7b034e49ee31df73e1ac4761
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-09-21 11:41:43 +0530

    changed patient.java

---
+++ src/main/java/com/cg/healthcaresystem/model/Patient.java
@@ -0,0 +1,38 @@
+package com.cg.healthcaresystem.model;
+
+
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer patientid;
+    @Size(min=2 , max=20)
+    @NotNull(message="Please enter your name")
+    private String name;
+    @Size(min=2 , max=20)
+    @NotNull(message ="Please enter your phone number")
+    private String phoneNo;
+
+    @NotNull(message="Please enter your age")
+    private Integer age;
+
+    @NotNull(message="Please enter the gender")
+    private String gender;
+
+    @OneToOne(targetEntity = Appointment.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "", referencedColumnName = "id")
+    private List<Appointment> AppointmentList;
+}

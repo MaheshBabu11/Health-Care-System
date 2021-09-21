7679b2039aa7ecb11acee33e3008e6accf3100dd
Mahesh Babu <crazymahesh98@gmail.com>
2021-09-21 11:26:46 +0530

    added model TestResult

---
+++ src/main/java/com/cg/healthcaresystem/model/TestResult.java
@@ -0,0 +1,22 @@
+package com.cg.healthcaresystem.model;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name="Test_Result")
+public class TestResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @NotNull(message = "Please enter test reading")
+    private double testReading;
+    @NotNull(message = "please enter condition")
+    private String condition;
+    @NotNull(message = "please enter appointment details")
+    private Appointment appointment;
+}

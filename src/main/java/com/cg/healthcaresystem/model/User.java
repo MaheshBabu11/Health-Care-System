beaf72faf20cb527645c00fbfa3dc79646ed4eb9
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-09-21 11:39:43 +0530

    changed user.java

--- src/main/java/com/cg/healthcaresystem/model/User.java
+++ src/main/java/com/cg/healthcaresystem/model/User.java
@@ -1,4 +1,31 @@
 package com.cg.healthcaresystem.model;
 
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.Size;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Size(min = 2, max = 20)
+    @NotNull(message = "Pls Enter User Name!")
+    private String username;
+    @Size(min = 2, max = 20)
+
+    @NotNull(message = "Pls Enter Password!")
+    private String password;
+
+    @NotNull(message = "Pls Enter role")
+    private String role;
+
 }

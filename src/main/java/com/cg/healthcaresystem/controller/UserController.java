3a8442513f6ec3cdbc3aa9a9dfdc07b607b35acc
Mahesh Babu <crazymahesh98@gmail.com>
2021-09-21 20:12:22 +0530

    added swagger support and admin controller

---
+++ src/main/java/com/cg/healthcaresystem/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.cg.healthcaresystem.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("User")
+public class UserController {
+
+}
---
+++ src/main/java/com/cg/healthcaresystem/service/UserService.java
@@ -0,0 +1,8 @@
+package com.cg.healthcaresystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+}

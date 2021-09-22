09e051459afaa487b29de9501370d657cb6f1664
dibbyorocks <dibbyobhattacharjee@gmail.com>
2021-09-22 16:07:33 +0530

    AppointmentRepository added

--- src/main/java/com/cg/healthcaresystem/repository/AppointmentRepository.java
+++ src/main/java/com/cg/healthcaresystem/repository/AppointmentRepository.java
@@ -4,9 +4,7 @@ import com.cg.healthcaresystem.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface AppointmentRepository extends JpaRepository<Appointment,Integer> {
+public interface AppointmentRepository extends JpaRepository <Appointment,Integer> {
 
 }

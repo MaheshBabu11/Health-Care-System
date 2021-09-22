e6c47a90d72874a13108b9502cce1bfdf5906dda
Koushik Dey <dey.98koushikdey@gmail.com>
2021-09-22 16:12:32 +0530

    update DiagnosticTestController,

--- src/main/java/com/cg/healthcaresystem/controller/DiagnosticTestController.java
+++ src/main/java/com/cg/healthcaresystem/controller/DiagnosticTestController.java
@@ -7,12 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,12 +19,41 @@ public class DiagnosticTestController {
     @Autowired
     private DiagnosticTestService diagnosticTestService;
 
+    @ApiOperation("Get all the test")
+    @GetMapping("/getall")
+
+    public List<DiagnosticTest> getAllTest(){
+        log.info("Fetch all the tests");
+        return diagnosticTestService.getAllTest();
+    }
+
     @ApiOperation("Add new Test")
     @PostMapping("/addtest")
-    public ResponseEntity<DiagnosticTest> addtest(@Valid @RequestBody DiagnosticTest test){
+    public ResponseEntity<DiagnosticTest> addNewTest(@Valid @RequestBody DiagnosticTest test){
         DiagnosticTest newTest = diagnosticTestService.addNewTest(test);
         log.info("Added new test");
         return new ResponseEntity<>(newTest, HttpStatus.CREATED);
+    }
+
+    @ApiOperation("")
+    @GetMapping("/gettestofdiagnosticcenter")
+    public DiagnosticTest getTestOfDiagnosticCenter(@PathVariable int centerId){
+
+        return null;
+    }
+
+    @ApiOperation("Update test")
+    @PutMapping("/updatetest")
+    public ResponseEntity<DiagnosticTest> updateTestDetails(@Valid @RequestBody DiagnosticTest test){
+
+        return null;
+    }
+
+    @ApiOperation("Remove test from diagnostic center")
+    @DeleteMapping("/removetest")
+    public ResponseEntity<DiagnosticTest> removeTestFormDiagnosticCenter(@Valid @RequestBody DiagnosticTest test){
+
+        return null;
     }
 
 }

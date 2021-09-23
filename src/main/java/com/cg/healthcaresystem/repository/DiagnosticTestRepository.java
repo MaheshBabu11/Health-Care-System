a7dd0742c5f3393c9b9c1726d77608b577ba999f
Koushik Dey <dey.98koushikdey@gmail.com>
2021-09-23 15:50:50 +0530

    Updated DiagnosticTestRepository

--- src/main/java/com/cg/healthcaresystem/repository/DiagnosticTestRepository.java
+++ src/main/java/com/cg/healthcaresystem/repository/DiagnosticTestRepository.java
@@ -1,11 +1,9 @@
 package com.cg.healthcaresystem.repository;
 
-import com.cg.healthcaresystem.model.DiagnosticCenter;
 import com.cg.healthcaresystem.model.DiagnosticTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface DiagnosticTestRepository extends JpaRepository<DiagnosticTest,Integer> {

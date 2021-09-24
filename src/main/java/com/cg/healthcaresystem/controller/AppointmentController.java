3e2b55358a065a0b781dd2546652752637b75335
dibbyorocks <dibbyobhattacharjee@gmail.com>
2021-09-24 12:40:00 +0530

    Appoinment controller functionality updated

--- src/main/java/com/cg/healthcaresystem/controller/AppointmentController.java
+++ src/main/java/com/cg/healthcaresystem/controller/AppointmentController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -31,5 +33,30 @@ public class AppointmentController {
     {
         Set<Appointment> appointments=appointmentService.viewAppointments(patientName);
         return new ResponseEntity<>(appointments,HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Appointment> removeAppointment(@RequestBody Appointment appointment)
+    {
+        Appointment appointments=appointmentService.removeAppointment(appointment);
+        return new ResponseEntity<>(appointments,HttpStatus.NO_CONTENT);
+    }
+    @GetMapping("/view by id")
+    public ResponseEntity<Optional<Appointment>> viewAppointment(@RequestBody int appointmentId)
+    {
+
+        Optional<Appointment> appointments =appointmentService.viewAppointment(appointmentId);
+        return new ResponseEntity<>(appointments,HttpStatus.UPGRADE_REQUIRED);
+    }
+    @PutMapping ("/update")
+    public ResponseEntity<Appointment> updateAppointment(@RequestBody Appointment appointment)
+    {
+        Appointment appointments =appointmentService.updateAppointment(appointment);
+        return new ResponseEntity<>(appointments,HttpStatus.UPGRADE_REQUIRED);
+    }
+    @GetMapping("view list of appointment  ")
+    public ResponseEntity<List<Appointment>> getAppointmentList(@RequestBody int centreId, @RequestBody String test, @RequestBody int Status)
+    {
+        return ResponseEntity.ok(this.appointmentService.getAppointmentList(centreId,test,Status));
     }
 }

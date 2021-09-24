b9ff49d36dec3790e7b6a6523ea1ac9532834113
Md Saifuddin <mdsaif.wb@gmail.com>
2021-09-24 10:18:34 +0530

    Updated DiagnosticCenterController

--- src/main/java/com/cg/healthcaresystem/controller/DiagnosticCenterController.java
+++ src/main/java/com/cg/healthcaresystem/controller/DiagnosticCenterController.java
@@ -1,11 +1,15 @@
 package com.cg.healthcaresystem.controller;
 
 import com.cg.healthcaresystem.model.DiagnosticCenter;
+import com.cg.healthcaresystem.model.DiagnosticTest;
 import com.cg.healthcaresystem.service.DiagnosticCenterService;
+import com.cg.healthcaresystem.service.DiagnosticTestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.*;
 public class DiagnosticCenterController {
     @Autowired
     private DiagnosticCenterService diagnosticCenterService;
+
+    @Autowired
+    private DiagnosticTestService diagnosticTestService;
 
     @PostMapping("/add")
     public ResponseEntity<DiagnosticCenter> add(@RequestBody DiagnosticCenter diagnosticCenter)
@@ -30,6 +37,42 @@ public class DiagnosticCenterController {
     @GetMapping("/getbyid/{id}")
     public DiagnosticCenter fetchById(@PathVariable Integer id) {
         return diagnosticCenterService.getDiagnosticCenterById(id);
+
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<DiagnosticCenter> update(@RequestBody DiagnosticCenter diagnosticCenter)
+    {
+        return ResponseEntity.ok(this.diagnosticCenterService.updateDiagnosticCenter(diagnosticCenter));
+    }
+
+    @GetMapping("/viewtest/{centerId}/{testName}")
+    public ResponseEntity<DiagnosticTest> viewTestDetails(@PathVariable Integer centerId,@PathVariable String testName)
+    {
+        return ResponseEntity.ok(this.diagnosticCenterService.viewTestDetails(centerId,testName));
+    }
+
+    @PostMapping("/addtest/{diagnosticcenterId}/{testid}")
+    public ResponseEntity<DiagnosticTest> addTest(@PathVariable Integer diagnosticcenterId, @PathVariable Integer testid)
+    {
+        return ResponseEntity.ok(this.diagnosticCenterService.addTest(diagnosticcenterId,testid));
+    }
+
+    @GetMapping("/getbyname/{name}")
+    public DiagnosticCenter fetchByName(@PathVariable String name) {
+        return diagnosticCenterService.getDiagnosticCenter(name);
+    }
+
+    @DeleteMapping("remove/{id}")
+    public List<DiagnosticCenter> removeDiagnosticCenter(@PathVariable Integer id)
+    {
+        return diagnosticCenterService.removeDiagnosticCenter(id);
+    }
+
+    @GetMapping("/gettestbyid/{id}")
+    public DiagnosticTest fetchTestById(@PathVariable Integer id) {
+        return diagnosticCenterService.getDiagnosticTestById(id);
+
     }
 
 }

1d352dece6f1ad4646793d23410bb0b56427401f
Mahesh Babu <crazymahesh98@gmail.com>
2021-09-28 16:01:29 +0530

    added exception package

---
+++ src/main/java/com/cg/healthcaresystem/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cg.healthcaresystem.exception;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}

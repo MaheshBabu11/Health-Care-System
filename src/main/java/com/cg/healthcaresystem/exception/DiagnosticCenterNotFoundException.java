1f5e6925442679d55cb6877572eb7651788a98fe
Md Saifuddin <mdsaif.wb@gmail.com>
2021-09-28 16:16:54 +0530

    Added DiagnosticCenterNotFoundException

---
+++ src/main/java/com/cg/healthcaresystem/exception/DiagnosticCenterNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cg.healthcaresystem.exception;
+
+public class DiagnosticCenterNotFoundException extends Exception {
+    public DiagnosticCenterNotFoundException(String message) {
+        super(message);
+    }
+}

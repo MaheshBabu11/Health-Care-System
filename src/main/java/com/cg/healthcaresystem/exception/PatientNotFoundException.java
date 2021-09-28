c84b5aac85f86b2cca3eae89da6a8e7b9819eaa9
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-09-28 16:03:55 +0530

    updated PatientNotFoundException

---
+++ src/main/java/com/cg/healthcaresystem/exception/PatientNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cg.healthcaresystem.exception;
+
+public class PatientNotFoundException extends  Exception {
+    PatientNotFoundException (String message){
+            super(message);
+        }
+    }
+

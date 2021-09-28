b3542718c1f024d64045baa10606ad4fb156b1a8
Koushik Dey <dey.98koushikdey@gmail.com>
2021-09-28 16:37:14 +0530

    Updated DiagnosticTestTestException

--- src/main/java/com/cg/healthcaresystem/service/DiagnosticTestServiceImpl.java
+++ src/main/java/com/cg/healthcaresystem/service/DiagnosticTestServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cg.healthcaresystem.service;
 
+import com.cg.healthcaresystem.exception.DiagnosticTestNotFoundException;
 import com.cg.healthcaresystem.model.DiagnosticCenter;
 import com.cg.healthcaresystem.model.DiagnosticTest;
 import com.cg.healthcaresystem.repository.DiagnosticCenterRepository;
 import com.cg.healthcaresystem.repository.DiagnosticTestRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class DiagnosticTestServiceImpl implements DiagnosticTestService {
 
@@ -41,25 +44,41 @@ public class DiagnosticTestServiceImpl implements DiagnosticTestService {
     @Override
     public DiagnosticTest updateTestDetails(DiagnosticTest test){
         DiagnosticTest dt = null;
+        try {
         Optional<DiagnosticTest> optionalDiagnosticTest = diagnosticTestRepository.findById(test.getId());
-        if(optionalDiagnosticTest.isPresent())
-            dt = optionalDiagnosticTest.get();
 
-        dt.setTestName(test.getTestName());
-        dt.setTestPrice(test.getTestPrice());
-        dt.setNormalValue(test.getNormalValue());
-        dt.setUnits(test.getUnits());
-        return diagnosticTestRepository.save(dt);
+            if (optionalDiagnosticTest.isPresent())
+                dt = optionalDiagnosticTest.get();
+            else{
+                throw new DiagnosticTestNotFoundException("Test not found");
+            }
+            dt.setTestName(test.getTestName());
+            dt.setTestPrice(test.getTestPrice());
+            dt.setNormalValue(test.getNormalValue());
+            dt.setUnits(test.getUnits());
+            return diagnosticTestRepository.save(dt);
+
+        }catch (DiagnosticTestNotFoundException e){
+            log.info("Diagnostic Test not found");
+            return null;
+        }
     }
 
     @Override
     public DiagnosticTest removeTestFormDiagnosticTest(DiagnosticTest test){
+        try{
         Optional<DiagnosticTest> optionalDiagnosticTest = diagnosticTestRepository.findById(test.getId());
+
         if(optionalDiagnosticTest.isPresent()){
             diagnosticTestRepository.deleteById(test.getId());
             return test;
-        }else {
+        }else{
+            throw new DiagnosticTestNotFoundException("Test not found");
+        }
+        }catch (DiagnosticTestNotFoundException e){
+            log.info("Diagnostic Test not found");
             return null;
         }
+
     }
 }

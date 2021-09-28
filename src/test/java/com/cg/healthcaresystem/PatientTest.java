d976e7e0cdcd192a9238954ec9c2ba740a792b73
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-09-28 09:51:23 +0530

    updated PatientTest

--- src/test/java/com/cg/healthcaresystem/PatientTest.java
+++ src/test/java/com/cg/healthcaresystem/PatientTest.java
@@ -12,6 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @SpringBootTest
 public class PatientTest {
 @Autowired
@@ -21,19 +24,22 @@ public class PatientTest {
     @Test
     public void createTest() {
 
-        Patient patient=new Patient("Mahesh","9605431794",25,"male");
+        Patient patient=new Patient("grv","9605431794",25,"male");
+        Patient patient2=new Patient("rohini","34566777",31,"female");
 
        Assert.assertEquals(patient,patientService.registerPatient(patient));
-
+        Assert.assertEquals(patient2,patientService.registerPatient(patient2));
 
     }
     @Test
     public void createTest2() {
 
-        Patient patient=new Patient("kaushik","860522294",21,"female");
-
+        Patient patient=new Patient("grv","860522294",null,null);
+        Patient patient2=new Patient(null,"860522294",null,"male");
+        Patient patient3=new Patient("grov",null,21,null);
         Assert.assertEquals(patient,patientService.registerPatient(patient));
-
+        Assert.assertEquals(patient,patientService.registerPatient(patient2));
+        Assert.assertEquals(patient,patientService.registerPatient(patient3));
 
     }
 }

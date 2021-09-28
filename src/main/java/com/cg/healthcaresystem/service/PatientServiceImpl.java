c982a0e13c181bf10e33b1e43a499c173e5f7401
Gaurav Kumar Mahato <gauravmahato178.gkm@gmail.com>
2021-09-28 12:56:06 +0530

    updated PatientServiceImpl

--- src/main/java/com/cg/healthcaresystem/service/PatientServiceImpl.java
+++ src/main/java/com/cg/healthcaresystem/service/PatientServiceImpl.java
@@ -9,10 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Iterator;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+
 
 @Slf4j
 @Service
